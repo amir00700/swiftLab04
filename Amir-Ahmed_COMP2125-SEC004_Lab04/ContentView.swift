ae52cdd934aacfbae71405a4904177a0352e6e70
Amir Ahmed <43377462+amir00700@users.noreply.github.com>
2021-03-16 20:09:52 -0400

    Add files via upload

---
+++ Amir-Ahmed_COMP2125-SEC004_Lab04/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Amir-Ahmed_COMP2125-SEC004_Lab04
+//
+//  Created by Amir Ahmed on 2020-07-19.
+//  Copyright © 2020 Amir Ahmed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
